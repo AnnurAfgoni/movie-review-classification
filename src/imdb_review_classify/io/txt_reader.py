9ae7fef0ff1c93905dccc60da8a27fc51086ff3f
AnnurAfgoni <afgoniannur@gmail.com>
2024-05-24 23:40:15 +0800

    feat: modify txt reader attribute #3

--- src/imdb_review_classify/io/txt_reader.py
+++ src/imdb_review_classify/io/txt_reader.py
@@ -3,17 +3,21 @@ from pathlib import Path
 
 class FileManager:
 
-    def __init__(self, filedir: str):
-        self.path = Path(filedir)
+    def __init__(self, input_path: str, target_path: str):
+        self.data_input = Path(input_path)
+        self.data_target = Path(target_path)
 
-    def read_input(self, filename: str):
-        _reviews_data = self.path.joinpath(filename)
+        self.input_data = self._read_input()
+        self.target_data = self._read_target()
+
+    def _read_input(self):
+        _reviews_data = self.data_input
         with open(_reviews_data, "r") as f:
             _reviews = f.read()
         return _reviews
 
-    def read_target(self, filename: str):
-        _target_data = self.path.joinpath(filename)
+    def _read_target(self):
+        _target_data = self.data_target
         with open(_target_data, "r") as f:
             _target = f.read()
         return _target
